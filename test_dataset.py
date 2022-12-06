244f007b772cbd78517b60b25c76f28b9f9a1b67
whatever60 <quym@foxmail.com>
2022-12-06 15:27:14 -0600

    modify dataset testing

--- test_dataset.py
+++ test_dataset.py
@@ -1,9 +1,9 @@
 from functools import partial
 import json
 
-from flatten_dict import flatten, unflatten
+import ml_collections as mlc
 
-from openfold.data import OpenFoldSingleDataset
+from openfold.data.data_modules import OpenFoldSingleDataset
 from test_datamodule import args, update_config
 
 
@@ -19,22 +19,32 @@ obsolete_pdbs_file_path = (
     "/scratch/00946/zzhang/data/openfold/ls6-tacc/pdb_mmcif/obsolete.dat"
 )
 
+train_data_dir = "/scratch/00946/zzhang/data/openfold/ls6-tacc/pdb_mmcif/mmcif_files"
+# train_data_dir = "/work/09101/whatever/data/example/alphafold"
 
-# train_data_dir = "/scratch/00946/zzhang/data/openfold/ls6-tacc/pdb_mmcif/mmcif_files"
-train_data_dir = "/work/09101/whatever/data/example/alphafold"
-train_alignment_dir = "/scratch/00946/zzhang/data/openfold/ls6-tacc/alignment_db"
+setting = "msa"
+if setting == "msa":
+    train_alignment_dir = "/scratch/00946/zzhang/data/openfold/ls6-tacc/alignment_db"
+    alignment_index_path = "/scratch/00946/zzhang/data/openfold/ls6-tacc/gustaf/chain_lists/duplicated_super_fix.index"
+    val_alignment_dir = "/scratch/00946/zzhang/data/openfold/ls6-tacc/gustaf/val_set/alignments"
+elif setting == "baseline":
+    train_alignment_dir = "/scratch/09101/whatever/data/openfold/alignment_db"
+    alignment_index_path = "/scratch/09101/whatever/data/openfold/alignment_db/duplicated_super_fix.index"
+    val_alignment_dir = "/scratch/09101/whatever/data/openfold/val_set"
+
 train_chain_data_cache_path = (
     "/scratch/00946/zzhang/data/openfold/ls6-tacc/gustaf/prot_data_cache.json"
 )
 train_filter_path = None
-alignment_index_path = "/scratch/00946/zzhang/data/openfold/ls6-tacc/gustaf/chain_lists/duplicated_super_fix.index"
 alignment_index = None
+val_data_dir = "/scratch/00946/zzhang/data/openfold/ls6-tacc/gustaf/val_set/data"
 
 if alignment_index_path is not None:
     with open(alignment_index_path, "r") as fp:
         alignment_index = json.load(fp)
-config = {}
 
+with open(f"configs/base.json") as f:
+    config = json.load(f)
 with open(f"configs/{args['config_stage']}.json") as f:
     config = update_config(config, json.load(f))
 if args["config_ptm"]:
@@ -46,6 +56,8 @@ if args["config_lowprec"]:
     with open(f"configs/low_prec.json") as f:
         config = update_config(config, json.load(f))
 
+config = mlc.ConfigDict(config)
+config = config["data"]
 dataset_gen = partial(
     OpenFoldSingleDataset,
     template_mmcif_dir=template_mmcif_dir,
@@ -61,9 +73,17 @@ train_dataset = dataset_gen(
     chain_data_cache_path=train_chain_data_cache_path,
     alignment_dir=train_alignment_dir,
     filter_path=train_filter_path,
-    max_template_hits=config.train.max_template_hits,
-    shuffle_top_k_prefiltered=config.train.shuffle_top_k_prefiltered,
+    max_template_hits=config["train"]["max_template_hits"],
+    shuffle_top_k_prefiltered=config["train"]["shuffle_top_k_prefiltered"],
     treat_pdb_as_distillation=False,
     mode="train",
     alignment_index=alignment_index,
+)
+
+eval_dataset = dataset_gen(
+    data_dir=val_data_dir,
+    alignment_dir=val_alignment_dir,
+    filter_path=None,
+    max_template_hits=config["eval"]["max_template_hits"],
+    mode="eval",
 )
